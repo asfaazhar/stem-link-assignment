35d11080cf52edc103b87d03d3a8631d4637b8aa
asfaazhar <157738967+asfaazhar@users.noreply.github.com>
2025-01-03 09:43:04 +0530

    week 3 files added

---
+++ Week-3/src/Navigation.jsx
@@ -0,0 +1,35 @@
+import { ShoppingCart } from "lucide-react";
+// import "./Navigation.css";
+
+function Navigation(props) {
+  return (
+    <nav className="flex items-center justify-between p-8 mx-16">
+      <div className="flex gap-x-16">
+        <a className="font-semibold text-3xl" href="/">
+          Mebius
+        </a>
+        <div className="flex items-center gap-4">
+          <a href="/">Home</a>
+          <a href="/shop">Shop</a>
+        </div>
+      </div>
+      <div className="flex items-center gap-4">
+        <div>
+          <a href="/cart" className="flex items-center gap-4 relative">
+            <p className="text-lg">{props.cartCount}</p>
+            <div className="flex items-center gap-2">
+              <ShoppingCart />
+              Cart
+            </div>
+          </a>
+        </div>
+        <p>
+          <a className="p-4" href="/#">Sign In</a>
+          <a href="/#">Sign Up</a>
+        </p>
+      </div>
+    </nav>
+  );
+};
+
+export default Navigation;
